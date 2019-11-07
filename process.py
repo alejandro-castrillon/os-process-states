fac820b320c30cab189b1bc1469e410938c4f4ec
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-11-06 19:42:47 -0500

    Add the progress property in the Process clases

--- process.py
+++ process.py
@@ -9,10 +9,11 @@ class Process:
     def activate(self, pid):
         self.pid = pid
         self.priority_level = random.choice(
-            ["VeryHigh", "High", "Medium", "Low"]
+            ['VeryHigh', 'High', 'Medium', 'Low']
         )
         self.memory = random.randint(100, 300)
         self.quantum = random.randint(10, 50)
+        self.progress = 0
         self.interaction = random.choice([True, False])
 
     def deactivate(self):
@@ -20,19 +21,20 @@ class Process:
         self.priority_level = None
         self.memory = None
         self.quantum = None
+        self.progress = None
         self.interaction = None
 
     def __str__(self):
-        string = f"Process(name={self.name}"
+        string = f'Process(name={self.name}'
         if self.pid:
             string += (
-                f", pid={self.pid}, "
-                f"priority_level={self.priority_level}, "
-                f"memory={self.memory}, "
-                f"quantum={self.quantum}, "
-                f"interaction={self.interaction}"
+                f', pid={self.pid}, '
+                f'priority_level={self.priority_level}, '
+                f'memory={self.memory}, '
+                f'quantum={self.quantum}, '
+                f'interaction={self.interaction}'
             )
-        return string + ")"
+        return string + ')'
 
     def __repr__(self):
         return str(self)
